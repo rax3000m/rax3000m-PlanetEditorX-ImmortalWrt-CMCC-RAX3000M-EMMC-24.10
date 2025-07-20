3424867f9d3957e6d0b36183f04c1066d208623b
ledudude <github@ledudu.net>
2025-07-20 13:00:27 +0800

    change master

--- scripts/24.10-6.6/diy-part1.sh
+++ scripts/24.10-6.6/diy-part1.sh
@@ -16,5 +16,5 @@
 # git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git package/luci-app-adguardhome
 
 
-echo 'src-git adguardhome https://github.com/AdguardTeam/AdGuardHome.git;master' >>feeds.conf.default
+# echo 'src-git adguardhome https://github.com/AdguardTeam/AdGuardHome.git;master' >>feeds.conf.default
 echo 'src-git luciadguardhome https://github.com/xiaoxiao29/luci-app-adguardhome.git;master' >>feeds.conf.default
